a5f6dffa6b67be99095cf2a189280b61e365fc81
Amanuel Fekadu <amanuelfekadueba@gmail.com>
2024-02-05 14:18:57 +0300

    updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,4 +263,32 @@
     }
   });
 
+  const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message')
+
+  const sendEmail = (e) =>{
+      e.preventDefault()
+
+              //serviceID - temeplet - #form - publickey
+
+      emailjs.sendForm("service_3sx6br3","template_701609j","#contact-form","QljGcqoO-jQXZ47IM")
+      .then(() =>{
+            // Show sent message 
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(()=>{
+                contactMessage.textContent = ''
+            }, 5000)
+
+            //Clear input Fields
+            contactForm.reset()
+
+      }, () =>{
+          // Show error message
+          contactMessage.textContent= 'Message not sent (service error) ❌'
+          })
+    }
+    contactForm.addEventListener('submit', sendEmail)
+
 })()
